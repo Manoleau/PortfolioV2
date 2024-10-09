cd3bf11ce1d3e3d4c88300031f403cb41299b215
Manolo <emmanuelar.pro@gmail.com>
2024-10-09 15:57:07 +0200

    views

---
+++ src/app/test-view/test-view.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-test-view',
+  standalone: true,
+  imports: [],
+  templateUrl: './test-view.component.html',
+  styleUrl: './test-view.component.css'
+})
+export class TestViewComponent {
+
+}
