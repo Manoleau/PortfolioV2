c77aea1bab000847707e0dff456480ec3a385977
Manolo <emmanuelar.pro@gmail.com>
2024-10-09 15:57:38 +0200

    config

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { HomeViewComponent } from './home-view/home-view.component';
+import { TestViewComponent } from './test-view/test-view.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeViewComponent },
+  { path: 'test', component: TestViewComponent },
+  { path: '**', redirectTo: '' }
+];
